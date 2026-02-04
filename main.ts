f2354b42e43c52b4779c37b083f7dbab81ca7fec
24196-ship-it <24196@greijdanus.nl>
2026-02-04 10:21:21 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,10 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+})
+radio.setGroup(1)
